9b069df34c6108bafd3f28e5d934008b37968758
meganm672 <meganm672@gmail.com>
2023-08-17 21:09:20 -0400

    completed all steps to complete the problem

---
+++ index.js
@@ -0,0 +1,43 @@
+// Write code that defines a constructor function called Car 
+// that creates a new object with 
+// the following properties: make, model, and year.
+class Car{
+    constructor(make,model,year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    // Add a method called getDescription to the Car prototype, 
+    // which returns a string containing information about the car.
+    getDescription(){
+        return `This is a ${this.make} ${this.model} made in ${this.year}.`
+    }
+}
+
+
+// Define the ElectricCar function as a subclass of Car. 
+// Include an additional property called range that represents 
+// the range of the electric car in miles.
+class ElectricCar extends Car{
+    constructor(make,model,year,range){
+        super(make,model,year);
+        this.range = range;
+    }
+    // Have the ElectricCar prototype override the 
+    // getDescription method of the Car prototype with a 
+    // new implementation that includes information about the range 
+    // of the electric car.
+    getDescription(){
+        return `${super.getDescription()} It can go ${this.range} miles before needing a charge.`
+    }
+}
+
+// Create an instance of ElectricCar with 
+//the make "Tesla", model "Model S", year 2019, and range 300, 
+//and the getDescription method is called on the instance. 
+// The output will be a string containing 
+// the make, model, year, and range of the electric car.
+
+const electricCar = new ElectricCar ("Tesla","Model S", 2019, 300);
+
+console.log(electricCar.getDescription());
